5e7875e60a356772918c20f3832fff6c6d719db4
megamilkx <gyrocopter3k@gmail.com>
2018-01-29 02:46:05 +0300

    Dontcare

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <windows.h>
 #include <dsound.h>
 
+#include <stdint.h>
+
 #include "gfxm.h"
 
 #define DIRECT_SOUND_CREATE(name) HRESULT WINAPI name(LPCGUID pcGuidDevice, LPDIRECTSOUND *ppDS, LPUNKNOWN pUnkOuter)
@@ -22,9 +24,11 @@ public:
         memcpy(this->data, data, bytes);
     }
     
+    size_t Size() { return bytes; }
+    
     void CopyData(void* data, size_t bytes, int cursor, gfxm::vec3 leftEarPos, gfxm::vec3 rightEarPos, gfxm::vec3 sourcePos)
     {
-        char* clip = (char*)data;
+        short* clip = (short*)data;
         int szAudio1 = this->bytes - cursor > bytes ? bytes : this->bytes - cursor;
         int szAudio2 = szAudio1 < bytes ? bytes - szAudio1 : 0;
         char* clip1 = (char*)this->data + cursor;
@@ -39,13 +43,19 @@ public:
         
         for(unsigned i = 0; i < szAudio1 /2 ; i += 2)
         {
-            ((short*)clip)[i] += ((short*)clip1)[i] * leftVol;
-            ((short*)clip)[i + 1] += ((short*)clip1)[i + 1] * rightVol;
+            short ls = ((short*)clip1)[i];
+            short rs = ((short*)clip1)[i + 1];
+            short sample = ls > rs ? ls : rs;
+            clip[i] += sample * leftVol;
+            clip[i + 1] += sample * rightVol;
         }
         for(unsigned i = 0; i < szAudio2 /2; i += 2)
         {
-            ((short*)clip)[szAudio1 + i] += ((short*)clip2)[i] * leftVol;
-            ((short*)clip)[szAudio1 + i + 1] += ((short*)clip2)[i + 1] * rightVol;
+            short ls = ((short*)clip2)[i];
+            short rs = ((short*)clip2)[i + 1];
+            short sample = ls > rs ? ls : rs;
+            clip[szAudio1 + i] += sample * leftVol;
+            clip[szAudio1 + i + 1] += sample * rightVol;
         }
     }
 private:
@@ -55,141 +65,67 @@ private:
     int channels;
 };
 
-struct AudioObject
+struct AudioEmitter
 {
+    AudioEmitter()
+    : buffer(0),
+    cursor(0),
+    stopped(true)
+    {}
+    
     AudioBuffer* buffer = 0;
-    int cursor = 0;
+    uint64_t cursor = 0;
     gfxm::vec3 position;
+    bool looping;
+    bool stopped;
+    
+    void Play(bool loop = false)
+    {
+        looping = loop;
+        stopped = false;
+        cursor = 0;
+    }
+    
+    bool IsStopped() { return stopped; }
+    bool IsLooped() { return looping; }
+    
+    void Mix(void* dest, size_t destSz, gfxm::vec3& leftEarPos, gfxm::vec3& rightEarPos)
+    {
+        if(!buffer)
+            return;
+        if(stopped)
+            return;
+        
+        buffer->CopyData(
+            dest, 
+            destSz, 
+            cursor,
+            leftEarPos,
+            rightEarPos,
+            position
+        );
+    }
+    
+    void Advance(int adv)
+    {
+        size_t bufSize = buffer->Size();
+        cursor += adv;
+        if(cursor >= bufSize)
+            cursor = cursor - bufSize;
+    }
 };
 
 class AudioMixer3D
 {
 public:
-    int Init()
-    {
-        HINSTANCE hInstance = GetModuleHandle(0);
-      
-        WNDCLASSEX wc;
-        wc.cbSize = sizeof(WNDCLASSEX);
-        wc.style = 0;
-        wc.lpfnWndProc = DefWindowProc;
-        wc.cbClsExtra = 0;
-        wc.cbWndExtra = 0;
-        wc.hInstance = hInstance;
-        wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-        wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-        wc.hbrBackground = (HBRUSH)(COLOR_WINDOW);
-        wc.lpszMenuName = NULL;
-        wc.lpszClassName = L"window";
-        wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-
-        if(!RegisterClassEx(&wc))
-        {
-            return false;
-        }
-
-        hWnd = CreateWindowExW(
-            0,
-            L"window",
-            L"hiddenwindow",
-            WS_OVERLAPPEDWINDOW,
-            CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720,
-            NULL, NULL, hInstance, NULL
-        );
-
-        ShowWindow(hWnd, SW_HIDE);
-        //ShowWindow(hWnd, SW_SHOW);
-        UpdateWindow(hWnd);
-        
-        WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 2, 44100, 44100 * 4, 4, 16, 0 };
-        
-        HRESULT hr;
-        HMODULE DSoundLib = LoadLibraryA("dsound.dll");
-        if(DSoundLib)
-        {
-            direct_sound_create* DirectSoundCreate = 
-                (direct_sound_create*)GetProcAddress(DSoundLib, "DirectSoundCreate");
-            
-            LPDIRECTSOUND DirectSound;
-            if(DirectSoundCreate && 
-                SUCCEEDED(DirectSoundCreate(0, &DirectSound, 0)))
-            {
-                hr = DirectSound->SetCooperativeLevel(hWnd, DSSCL_PRIORITY);
-                if(FAILED(hr))
-                {
-                    std::cout << "SetCooperativeLevel failed: " << std::hex << hr << std::endl;
-                    return 1;
-                }
-                
-                DSBUFFERDESC BuffDesc = { 0 };
-                BuffDesc.dwSize = sizeof(BuffDesc);
-                BuffDesc.dwFlags = DSBCAPS_PRIMARYBUFFER;
-                
-                hr = DirectSound->CreateSoundBuffer(&BuffDesc, &PrimaryBuffer, 0);
-                if(FAILED(hr))
-                {
-                    std::cout << "CreateSoundBuffer failed (primary): " << std::hex << hr << std::endl;
-                    return 1;
-                }
-                hr = PrimaryBuffer->SetFormat(&wfx);
-                if(FAILED(hr))
-                {
-                    std::cout << "SetFormat failed (primary): " << std::hex << hr << std::endl;
-                    return 1;
-                }
-                
-                BuffDesc.dwFlags = DSBCAPS_GLOBALFOCUS;
-                BuffDesc.dwBufferBytes = 44100 * 2;
-                BuffDesc.lpwfxFormat = &wfx;
-                hr = DirectSound->CreateSoundBuffer(&BuffDesc, &SecondaryBuffer, 0);
-                if(FAILED(hr))
-                {
-                    std::cout << "CreateSoundBuffer failed (secondary): " << std::hex << hr << std::endl;
-                    return 1;
-                }
-                
-                void* audio1;
-                DWORD szAudio1;
-                void* audio2;
-                DWORD szAudio2;
-                hr = SecondaryBuffer->Lock(
-                    0, 0,
-                    &audio1, &szAudio1,
-                    &audio2, &szAudio2,
-                    DSBLOCK_ENTIREBUFFER 
-                );
-                ZeroMemory(audio1, szAudio1);
-                ZeroMemory(audio2, szAudio2);
-                SecondaryBuffer->Unlock(
-                    audio1, szAudio1,
-                    audio2, szAudio2
-                );
-                
-                SecondaryBuffer->Play(0, 0, DSBPLAY_LOOPING);
-            }
-            else
-            {
-                std::cout << "DirectSoundCreate failed" << std::endl;
-                return 1;
-            }
-        }
-        
-        return 0;
-    }
-    
+    int Init();
     void Cleanup();
     
     void EarDistance(float meters) { earDistance = meters; }
     float EarDistance() { return earDistance; }
     
-    MSG msg;
     void Update()
     {
-        while(PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
         HRESULT hr;
         DWORD writePos = 0;
         hr = SecondaryBuffer->GetCurrentPosition(NULL, &writePos);
@@ -203,7 +139,7 @@ public:
         void* audio2;
         DWORD szAudio2;
         hr = SecondaryBuffer->Lock(
-            writePos, 8820,
+            writePos, 10000,
             &audio1, &szAudio1,
             &audio2, &szAudio2,
             0 
@@ -213,28 +149,35 @@ public:
             std::cout << "Buffer lock failed: " << std::hex << hr << std::endl;
         }
         
-        
-        
         DWORD dPos;
         if(writePos >= prevWritePos)
             dPos = writePos - prevWritePos;
         else
         {
-            dPos = (44100 * 2 - prevWritePos) + writePos;
+            dPos = (44100 * 8 - prevWritePos) + writePos;
         }
         
         ZeroMemory(buffer, sizeof(buffer));
         //audioModule.Update((void*)buffer, 4410, dPos / 2);
-        for(AudioObject* obj : objects)
+        for(AudioEmitter* obj : objects)
         {
+            obj->Mix(
+                (void*)buffer, 
+                sizeof(buffer), 
+                gfxm::vec3(-0.1075f, 0.0f, 0.0f),
+                gfxm::vec3(0.1075f, 0.0f, 0.0f)
+            );
+            obj->Advance(dPos);
+            /*
             obj->buffer->CopyData(
                 (void*)buffer, 
-                4410, 
+                10000, 
                 obj->cursor += dPos,
                 gfxm::vec3(-0.1075f, 0.0f, 0.0f),
                 gfxm::vec3(0.1075f, 0.0f, 0.0f),
                 obj->position
             );
+            */
         }
         prevWritePos = writePos;
         
@@ -265,13 +208,13 @@ public:
             }
     }
     
-    AudioObject* CreateInstance()
+    AudioEmitter* CreateInstance()
     {
-        AudioObject* obj = new AudioObject();
+        AudioEmitter* obj = new AudioEmitter();
         objects.push_back(obj);
         return obj;
     }
-    void DestroyObject(AudioObject* obj)
+    void DestroyObject(AudioEmitter* obj)
     {
         for(unsigned i = 0; i < objects.size(); ++i)
             if(objects[i] == obj)
@@ -284,7 +227,7 @@ public:
 private:
     float earDistance = 0.215f;
     std::vector<AudioBuffer*> buffers;
-    std::vector<AudioObject*> objects;
+    std::vector<AudioEmitter*> objects;
 
     short buffer[88200];
     LPDIRECTSOUNDBUFFER PrimaryBuffer;
@@ -294,8 +237,141 @@ private:
     HWND hWnd;
 };
 
+int AudioMixer3D::Init()
+{
+    HINSTANCE hInstance = GetModuleHandle(0);
+  
+    WNDCLASSEX wc;
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = 0;
+    wc.lpfnWndProc = DefWindowProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = L"window";
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+    if(!RegisterClassEx(&wc))
+    {
+        return false;
+    }
+
+    hWnd = CreateWindowExW(
+        0,
+        L"window",
+        L"hiddenwindow",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720,
+        NULL, NULL, hInstance, NULL
+    );
+
+    ShowWindow(hWnd, SW_HIDE);
+    //ShowWindow(hWnd, SW_SHOW);
+    UpdateWindow(hWnd);
+    
+    WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 2, 44100, 44100 * 4, 4, 16, 0 };
+    
+    HRESULT hr;
+    HMODULE DSoundLib = LoadLibraryA("dsound.dll");
+    if(DSoundLib)
+    {
+        direct_sound_create* DirectSoundCreate = 
+            (direct_sound_create*)GetProcAddress(DSoundLib, "DirectSoundCreate");
+        
+        LPDIRECTSOUND DirectSound;
+        if(DirectSoundCreate && 
+            SUCCEEDED(DirectSoundCreate(0, &DirectSound, 0)))
+        {
+            hr = DirectSound->SetCooperativeLevel(hWnd, DSSCL_PRIORITY);
+            if(FAILED(hr))
+            {
+                std::cout << "SetCooperativeLevel failed: " << std::hex << hr << std::endl;
+                return 1;
+            }
+            
+            DSBUFFERDESC BuffDesc = { 0 };
+            BuffDesc.dwSize = sizeof(BuffDesc);
+            BuffDesc.dwFlags = DSBCAPS_PRIMARYBUFFER;
+            
+            hr = DirectSound->CreateSoundBuffer(&BuffDesc, &PrimaryBuffer, 0);
+            if(FAILED(hr))
+            {
+                std::cout << "CreateSoundBuffer failed (primary): " << std::hex << hr << std::endl;
+                return 1;
+            }
+            hr = PrimaryBuffer->SetFormat(&wfx);
+            if(FAILED(hr))
+            {
+                std::cout << "SetFormat failed (primary): " << std::hex << hr << std::endl;
+                return 1;
+            }
+            
+            BuffDesc.dwFlags = DSBCAPS_GLOBALFOCUS;
+            BuffDesc.dwBufferBytes = 44100 * 8;
+            BuffDesc.lpwfxFormat = &wfx;
+            hr = DirectSound->CreateSoundBuffer(&BuffDesc, &SecondaryBuffer, 0);
+            if(FAILED(hr))
+            {
+                std::cout << "CreateSoundBuffer failed (secondary): " << std::hex << hr << std::endl;
+                return 1;
+            }
+            
+            void* audio1;
+            DWORD szAudio1;
+            void* audio2;
+            DWORD szAudio2;
+            hr = SecondaryBuffer->Lock(
+                0, 0,
+                &audio1, &szAudio1,
+                &audio2, &szAudio2,
+                DSBLOCK_ENTIREBUFFER 
+            );
+            ZeroMemory(audio1, szAudio1);
+            ZeroMemory(audio2, szAudio2);
+            SecondaryBuffer->Unlock(
+                audio1, szAudio1,
+                audio2, szAudio2
+            );
+            
+            SecondaryBuffer->Play(0, 0, DSBPLAY_LOOPING);
+        }
+        else
+        {
+            std::cout << "DirectSoundCreate failed" << std::endl;
+            return 1;
+        }
+    }
+    
+    return 0;
+}
+
+void AudioMixer3D::Cleanup()
+{
+    
+}
+
 extern "C"{
 #include "stb_vorbis.h"
+}
+
+AudioBuffer* LoadAudioBuffer(AudioMixer3D* mixer, const std::string& name)
+{
+    AudioBuffer* buffer = mixer->CreateBuffer();
+    
+    int channels = 2;
+    int sampleRate = 44100;
+    short* decoded;
+    int len;
+    len = stb_vorbis_decode_filename(name.c_str(), &channels, &sampleRate, &decoded);
+    
+    buffer->Upload((void*)decoded, len * sizeof(short), sampleRate, channels);
+    free(decoded);
+    
+    return buffer;
 }
 
 int main()
@@ -303,31 +379,22 @@ int main()
     AudioMixer3D mixer;
     mixer.Init();
     
-    int channels = 2;
-    int sampleRate = 44100;
-    short* decoded;
-    int len;
-    len = stb_vorbis_decode_filename("test.ogg", &channels, &sampleRate, &decoded);
-    
-    AudioBuffer* buffer = mixer.CreateBuffer();
-    buffer->Upload((void*)decoded, len * sizeof(short), sampleRate, channels);
-    AudioObject* obj = mixer.CreateInstance();
-    obj->buffer = buffer;
+    AudioEmitter* obj = mixer.CreateInstance();
+    obj->buffer = LoadAudioBuffer(&mixer, "test.ogg");
     obj->position = gfxm::vec3(0.2f, 0.2f, -0.65f);
-    free(decoded);
-    
-    len = stb_vorbis_decode_filename("test2.ogg", &channels, &sampleRate, &decoded);
-    buffer = mixer.CreateBuffer();
-    buffer->Upload((void*)decoded, len * sizeof(short), sampleRate, channels);
+    obj->Play();
     obj = mixer.CreateInstance();
-    obj->buffer = buffer;
-    obj->position = gfxm::vec3(-0.2f, -0.2f, 0.65f);
-    free(decoded);
+    obj->buffer = LoadAudioBuffer(&mixer, "test2.ogg");
     
+    
+    
+    float pos = 0.0f;
     while(1)
     {
+        pos += 0.05f;
+        obj->position = gfxm::vec3(sinf(pos) * 0.4f, -0.2f, 0.1f);
         mixer.Update();
-        Sleep(10);
+        Sleep(5);
     }
     return 0;
 }
